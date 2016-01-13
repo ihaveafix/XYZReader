131bdcf918f9185086e20fa5fc051a902b5c1b54
Jill Hickman <jillhickman@gmail.com>
2016-01-12 22:12:11 -0700

    Moved FAB

--- src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -3,12 +3,14 @@ package com.example.xyzreader.ui;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
@@ -86,6 +88,18 @@ public class ArticleDetailActivity extends AppCompatActivity
             }
         });
 
+        //FAB button
+        final ArticleDetailActivity articleDetailActivity = this;
+        findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(articleDetailActivity)
+                        .setType("text/plain")
+                        .setText("Some sample text")
+                        .getIntent(), getString(R.string.action_share)));
+            }
+        });
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             mUpButtonContainer.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
                 @Override
@@ -105,6 +119,7 @@ public class ArticleDetailActivity extends AppCompatActivity
                 mSelectedItemId = mStartId;
             }
         }
+
     }
 
     @Override
